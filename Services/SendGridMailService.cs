b30f1cf8f7dab82f322ebe0c15f86c23263fe696
Rodrigo Cotting <cottingfontes@hotmail.com>
2025-03-17 01:12:37 -0300

    Adicionar arquivos de projeto.

---
+++ Services/SendGridMailService.cs
@@ -0,0 +1,6 @@
+﻿namespace modulum.Infrastructure.Shared.Services
+{
+    internal class SendGridMailService
+    {
+    }
+}
---
+++ Services/SystemDateTimeService.cs
@@ -0,0 +1,10 @@
+﻿using modulum.Application.Interfaces.Services;
+using System;
+
+namespace modulum.Infrastructure.Shared.Services
+{
+    public class SystemDateTimeService : IDateTimeService
+    {
+        public DateTime NowUtc => DateTime.UtcNow;
+    }
+}
